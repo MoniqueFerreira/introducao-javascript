31c843161ecbd181866249bf223b21faa18a05d7
MoniqueFerreira <moniquesz23@gmail.com>
2018-08-23 20:20:47 -0300

    terceira aula javascript

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,3 +4,9 @@ function trocaImagem() {
 function imagemTroca() {
 	document.getElementById("myImage").src="img/rc2.gif";
 }	
+function trocaFonte() {
+	document.getElementById("demo").style.fontSize="35px";
+}
+function fonteTroca() {
+	document.getElementById("demo").style.fontSize="16px";
+}
