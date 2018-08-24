392df777f68523b1e4996b0f3ffd880a736cde8c
MoniqueFerreira <moniquesz23@gmail.com>
2018-08-23 22:19:06 -0300

    terceira aula javascript

--- js/funcoes.js
+++ js/funcoes.js
@@ -8,5 +8,18 @@ function trocaFonte() {
 	document.getElementById("demo").style.fontSize="35px";
 }
 function fonteTroca() {
-	document.getElementById("demo").style.fontSize="16px";
+	document.getElementById("demo").style.fontSize="25px";
 }
+function ocultarElemento() {
+	document.getElementById("demo").style.display="none";
+}
+function mostrarElemento() {
+	document.getElementById("demo").style.display="block";
+}
+function calcular() {
+	document.getElementById("demo").innerHTML= document.getElementById("num1").value * document.getElementById("num2").value;
+}
+function calcular() {
+	document.getElementById("demo").innerHTML= parseFloat(document.getElementById("num3").value) + parseFloat(document.getElementById("num4").value);
+}
+
