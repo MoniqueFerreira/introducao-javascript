a5147f83641e35cb516af2c5d86730cd5a918ef4
MoniqueFerreira <42048828+MoniqueFerreira@users.noreply.github.com>
2018-08-26 18:42:05 -0300

    Com principais operações

--- js/funcoes.js
+++ js/funcoes.js
@@ -17,11 +17,18 @@ function mostrarElemento() {
 	document.getElementById("demo").style.display="block";
 }
 function calcular1() {
-	var total2 = document.getElementById("num1").value * document.getElementById("num2").value;
-	window.alert(total2);
+	var total1 = document.getElementById("num1").value * document.getElementById("num2").value;
+	window.alert(total1);
 }
 function calcular2() {
-	var total = parseFloat(document.getElementById("num3").value) + parseFloat(document.getElementById("num4").value);
-	window.alert(total);
+	var total2 = parseFloat(document.getElementById("num3").value) + parseFloat(document.getElementById("num4").value);
+	window.alert(total2);
 }
-
+function calcular3() {
+	var total3 = document.getElementById("num5").value - document.getElementById("num6").value;
+	window.alert(total3);
+}
+function calcular4() {
+	var total4 = document.getElementById("num7").value / document.getElementById("num8").value;
+	window.alert(total4);
+}
