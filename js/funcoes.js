64f98de917c38cc3be122856e1512a212d82be5e
Aluno <moniquesz23@gmail.com>
2018-08-30 21:42:40 -0300

    Formulário

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,11 +4,11 @@ function trocaImagem() {
 function imagemTroca() {
 	document.getElementById("myImage").src="img/rc2.gif";
 }	
-function trocaFonte() {
-	document.getElementById("demo").style.fontSize="35px";
-}
 function fonteTroca() {
-	document.getElementById("demo").style.fontSize="25px";
+	document.getElementById("demo").style.fontSize="16px";
+}
+function trocaFonte() {
+	document.getElementById("demo").style.fontSize="22px";
 }
 function ocultarElemento() {
 	document.getElementById("demo").style.display="none";
