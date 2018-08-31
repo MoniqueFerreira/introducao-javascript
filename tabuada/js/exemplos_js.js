e8aa1d8312a219785851391b54fe1d7f3021a538
Aluno <moniquesz23@gmail.com>
2018-08-30 22:38:26 -0300

    Formulário

---
+++ tabuada/js/exemplos_js.js
@@ -0,0 +1,10 @@
+//Escrever os números de 0 a 10 com laço de repetição
+var i;
+for(i = 0; i<=10; i++) {
+		document.write(i + " ");
+}
+//Tabuada 2
+var i;
+for(i = 0 ; i <=10; i ++) {
+		document.write(i * " ")
+}		
