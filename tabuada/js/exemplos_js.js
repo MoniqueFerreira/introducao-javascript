8480373e69cb564a7b626eaf660ad0c9810e654f
MoniqueFerreira <42048828+MoniqueFerreira@users.noreply.github.com>
2018-09-02 20:06:47 -0300

    tabuada até o 10

--- tabuada/js/exemplos_js.js
+++ tabuada/js/exemplos_js.js
@@ -1,11 +1,63 @@
 //Escrever os números de 0 a 10 com laço de repetição
 var i;
 for(i = 0; i<=10; i++) {
-		document.write(i + " ");
-}
+		document.writeln(i + " ");
+}	
+{
+	document.write("<br>");
+	document.write("<br>");
+}	
 //Tabuada 2
 var i;
-for(i = 0 ; i <=10; i ++) {
-		document.write(i * " ")
-		
-}		
+for(i=0; i<=10; i++) {
+	document.writeln("2 x " + i + " = " + (i*2) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 3
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("3 x " + i + " = " + (i*3) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 4
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("4 x " + i + " = " + (i*4) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 5
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("5 x " + i + " = " + (i*5) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 6
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("6 x " + i + " = " + (i*6) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 7
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("7 x " + i + " = " + (i*7) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 8
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("8 x " + i + " = " + (i*8) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 9
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("9 x " + i + " = " + (i*9) + "<br>");
+	document.write("<br>");
+}
+//Tabuada 10
+var i;
+for(i=0; i<=10; i++) {
+	document.writeln("10 x " + i + " = " + (i*10) + "<br>");
+	document.write("<br>");
+}
